feef99a071aa2a5e0b8db00898f85e6689aba3b3
Carlos Ricardo Machado Oliveira <carlos-ricardo.oliveira@serpro.gov.br>
2024-12-16 20:21:17 -0300

    adding key replace to predict

--- starter/test_main.py
+++ starter/test_main.py
@@ -12,8 +12,7 @@ def test_get_root():
     assert r.json() == "Welcome to the Census income class predictor API!"
 
 
-def test_post():
-    """
+def test_post_le():
     data = [{
         "age": 39,
         "workclass": "State-gov",
@@ -29,48 +28,13 @@ def test_post():
         "capital_loss": 0,
         "hours_per_week": 40,
         "native_country": "United-States"
-    }]
-    """
-    data = {
-        "age": 39,
-        "workclass": "State-gov",
-        "fnlgt": 77516,
-        "education": "Bachelors",
-        "education_num": 13,
-        "marital_status": "Never-married",
-        "occupation": "Adm-clerical",
-        "relationship": "Not-in-family",
-        "race": "White",
-        "sex": "Female",
-        "capital_gain": 2174,
-        "capital_loss": 0,
-        "hours_per_week": 40,
-        "native_country": "United-States"
-        }
+        }]
     r = TestClient(app).post("/", json=data)
     assert r.status_code == 200
     assert r.json() == "<=50K"
 
 
-def test_post2():
-    '''
-    data = [{
-        "age": 30,
-        "workclass": "State-gov",
-        "fnlgt": 141297,
-        "education": "Bachelors",
-        "education_num": 13,
-        "marital_status": "Married-civ-spouse",
-        "occupation": "Prof-specialty",
-        "relationship": "Husband",
-        "race": "Asian-Pac-Islander",
-        "sex": "Male",
-        "capital_gain": 0,
-        "capital_loss": 0,
-        "hours_per_week": 40,
-        "native_country": "India"
-    }]
-    '''
+def test_post_gt():
     data = {
         "age": 30,
         "workclass": "State-gov",
