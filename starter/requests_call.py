51dbd57629d244f15b4c6a855f4005cf6706e35d
Carlos Ricardo Machado Oliveira <carlos-ricardo.oliveira@serpro.gov.br>
2024-12-16 13:26:10 -0300

    FIX: sklearn version should be 1.3.2

--- starter/requests_call.py
+++ starter/requests_call.py
@@ -2,24 +2,63 @@ import requests
 import json
 
 url = 'https://nd0821-c3-machado.onrender.com/'
-census = {
-    "age": 30,
-    "workclass": "State-gov",
-    "fnlgt": 141297,
-    "education": "Bachelors",
-    "education_num": 13,
-    "marital_status": "Married-civ-spouse",
-    "occupation": "Prof-specialty",
-    "relationship": "Husband",
-    "race": "Asian-Pac-Islander",
-    "sex": "Male",
-    "capital_gain": 0,
-    "capital_loss": 0,
-    "hours_per_week": 40,
-    "native_country": "India"
-    }
+census = {"age": 30,
+        "workclass": "State-gov",
+        "fnlgt": 141297,
+        "education": "Bachelors",
+        "education_num": 13,
+        "marital_status": "Married-civ-spouse",
+        "occupation": "Prof-specialty",
+        "relationship": "Husband",
+        "race": "Asian-Pac-Islander",
+        "sex": "Male",
+        "capital_gain": 0,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "India"
+        }
 
-response = requests.post(url, data=json.dumps(census))
 
+
+print('#############################################')
+print('/ GET')
+response = requests.get(url)
 print('status code:', response.status_code)
 print('salary:', response.json())
+print('Response Headers:', response.headers)
+print('text:', response.text)
+
+print('#############################################')
+print('/ POST')
+headers = {'Content-Type': 'application/json'}
+#  response = requests.post(url, data=json.dumps(census), headers=headers)
+response = requests.post(url, json=census)
+
+print('status code:', response.status_code)
+# print('salary:', response.json())
+print("Request Headers:", response.request.headers)
+print("Request Body:", response.request.body)
+print('Response Headers:', response.headers)
+print('text:', response.text)
+print('more: ', response.reason)
+
+def test_post2():
+    data = [{
+        "age": 30,
+        "workclass": "State-gov",
+        "fnlgt": 141297,
+        "education": "Bachelors",
+        "education_num": 13,
+        "marital_status": "Married-civ-spouse",
+        "occupation": "Prof-specialty",
+        "relationship": "Husband",
+        "race": "Asian-Pac-Islander",
+        "sex": "Male",
+        "capital_gain": 0,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "India"
+    }]
+    r = TestClient(app).post("/", json=data)
+    assert r.status_code == 200
+    assert r.json() == ">50K"
