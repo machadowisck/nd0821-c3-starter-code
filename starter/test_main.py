828f8c5045d74cabc6cdaf2914599a5dcef01230
Carlos Ricardo Machado Oliveira <carlos-ricardo.oliveira@serpro.gov.br>
2024-12-16 14:46:29 -0300

    equalize test_main and request call data payload

--- starter/test_main.py
+++ starter/test_main.py
@@ -13,6 +13,7 @@ def test_get_root():
 
 
 def test_post():
+    """
     data = [{
         "age": 39,
         "workclass": "State-gov",
@@ -29,12 +30,46 @@ def test_post():
         "hours_per_week": 40,
         "native_country": "United-States"
     }]
+    """
+    data = {"age": 39,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education_num": 13,
+        "marital_status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Female",
+        "capital_gain": 2174,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "United-States"
+        }
     r = TestClient(app).post("/", json=data)
     assert r.status_code == 200
     assert r.json() == "<=50K"
 
 
 def test_post2():
+    '''
+    data = [{
+        "age": 30,
+        "workclass": "State-gov",
+        "fnlgt": 141297,
+        "education": "Bachelors",
+        "education_num": 13,
+        "marital_status": "Married-civ-spouse",
+        "occupation": "Prof-specialty",
+        "relationship": "Husband",
+        "race": "Asian-Pac-Islander",
+        "sex": "Male",
+        "capital_gain": 0,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "India"
+    }]
+    '''
     data = [{
         "age": 30,
         "workclass": "State-gov",
