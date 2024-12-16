173142693127830c86ee04130cdf3abd9f6c7b31
Carlos Ricardo Machado Oliveira <carlos-ricardo.oliveira@serpro.gov.br>
2024-12-15 23:09:41 -0300

    API deployment steps

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,70 @@
 # Put the code for your API here.
+
+import os
+import pickle
+import pandas as pd
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, Field
+from starter.ml.data import process_data
+from starter.ml.model import inference
+from enum import Enum
+from typing import List
+
+
+
+app = FastAPI()
+file_dir = os.path.dirname(__file__)
+model_path = os.path.join(file_dir, 'model/rf_model.pkl')
+encoder_path = os.path.join(file_dir, 'model/encoder.pkl')
+lb_path = os.path.join(file_dir, 'model/lb.pkl')
+model = pickle.load(open(model_path, 'rb'))
+encoder = pickle.load(open(encoder_path, 'rb'))
+lb = pickle.load(open(lb_path, 'rb'))
+
+class Census(BaseModel):
+    # Using the first row of census.csv as sample
+    age: int = Field(None, example=40)
+    workclass: str = Field(None, example='State-gov')
+    fnlgt: int = Field(None, example=88516)
+    education: str = Field(None, example='Bachelors')
+    education_num: int = Field(None, alias='education-num', example=13)
+    marital_status: str = Field(None, alias='marital-status', example='Never-married')
+    occupation: str = Field(None, example='Adm-clerical')
+    relationship: str = Field(None, example='Not-in-family')
+    race: str = Field(None, example='Black')
+    sex: str = Field(None, example='Female')
+    capital_gain: int = Field(None, alias='capital-gain', example=2174)
+    capital_loss: int = Field(None, alias='capital-loss', example=0)
+    hours_per_week: int = Field(None, alias='hours-per-week', example=40)
+    native_country: str = Field(None, alias='native-country', example='Peru')
+
+@app.get("/")
+async def home_page():
+    return "Welcome to the Census income class predictor API!"
+    
+    
+@app.post("/")
+async def predict(person: Census):
+    
+    data = pd.DataFrame.from_dict(person)
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X, _, _, _ = process_data(
+        data,
+        categorical_features=cat_features,
+        label=None,
+        training=False,
+        encoder=encoder,
+        lb=lb
+    )
+    pred = inference(model, X)[0]
+    return '<=50K' if pred == 0 else '>50K'
---
+++ starter/test_main.py
@@ -0,0 +1,51 @@
+import json
+from fastapi.testclient import TestClient
+from main import app
+
+def test_get_root():
+    r = TestClient(app).get("/")
+    assert r.status_code == 200
+    assert r.json() == "Welcome to the Census income class predictor API!"
+
+def test_post():
+    data = {
+        "age": 40,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education_num": 13,
+        "marital_status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Female",
+        "capital_gain": 2174,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "United-States"
+    }
+    r = TestClient(app).post("/", data=json.dumps(data))
+    assert r.status_code == 200
+    assert r.json() == "<=50K"
+
+
+def test_post2():
+    data = {
+        'age': 32,
+        'workclass': 'Private',
+        'fnlgt': 114937,
+        'education': 'Assoc-acdm',
+        'education-num': 13,
+        'marital-status': 'Married-civ-spouse',
+        'occupation': 'Adm-clerical',
+        'relationship': 'Husband',
+        'race': 'White',
+        'sex': 'Male',
+        'capital-gain': 0,
+        'capital-loss': 0,
+        'hours-per-week': 40,
+        'native-country': 'United-States'
+    }
+    r = TestClient(app).post("/", data=json.dumps(data))
+    assert r.status_code == 200
+    assert r.json() == ">50K"    
